23901370fd7314bcdda950491d9a8bf44542f5b1
jac2107 <jhoanarrrateacamara@gmail.com>
2026-07-28 16:55:12 -0500

    Release v1.0.7

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         applicationId = "com.example.musicapp"
-        minSdk = 23
+        minSdk = 24
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
